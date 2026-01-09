e17a3119bd08d7cbded78858f76d3b31ecf32fe6
min <theblueprint1015@gmail.com>
2026-01-09 17:58:56 +0900

    import calculator

---
+++ Python/Basic/Module/calculator_module.py
@@ -0,0 +1,22 @@
+# 모듈 : 다른 파이썬 프로그램에서 사용할 수 있는 코드
+
+# 더하기
+def add(x, y):
+    return x + y
+
+
+# 빼기
+def subtract(x, y):
+    return x - y
+
+
+# 곱하기
+def multiply(x, y):
+    return x * y
+
+
+# 나누기
+def divide(x, y):
+    return x / y
+
+
---
+++ Python/Basic/Module/run.py
@@ -0,0 +1,4 @@
+import calculator
+
+print(calculator.add(3, 5))
+print(calculator.multiply(4, 6))
