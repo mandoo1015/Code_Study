344cfe3f36e536176e10f09e327c0c77097bb07c
min <theblueprint1015@gmail.com>
2026-01-21 20:46:51 +0900

    삽입 정렬 코드 추가

---
+++ Python/알고리즘/탐색_정렬/insertion_sort.py
@@ -0,0 +1,8 @@
+def insertion_sort(data):
+    for i in range(1, len(data)):
+        key = data[i]
+        j = i - 1
+        while j >= 0 and data[j] > key:
+            data[j+1] = data[j] 
+            j -= 1
+        data[j+1] = key
